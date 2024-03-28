fcd0ad8e571aa7390ec1e909b16f0ae1d641a25e
Aastha-Panwar-141 <asthap@shriffle.com>
2024-03-28 19:28:46 +0530

    added new model and tried turbo frame

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_03_27_070138) do
+ActiveRecord::Schema[7.0].define(version: 2024_03_28_114557) do
   create_table "employees", force: :cascade do |t|
     t.string "employee_name"
     t.string "gender"
@@ -22,6 +22,12 @@ ActiveRecord::Schema[7.0].define(version: 2024_03_27_070138) do
   create_table "news", force: :cascade do |t|
     t.string "title"
     t.string "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "players", force: :cascade do |t|
+    t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
